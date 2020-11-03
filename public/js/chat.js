b721dd0073461465d914b2b0c50c6cce84d826fb
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-03 20:11:29 +0530

    Button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,6 @@
 const socket = io()
 
+// Connections
 socket.on('greetings', (data) => {
     console.log(data)
 })
@@ -12,24 +13,38 @@ socket.on('broadcastMessage', (msg) => {
 socket.on('sendLocation', (coords) => {
     console.log(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
 })
-document.querySelector('#msg-form').addEventListener('submit', (event) => {
+
+
+// Elements
+const $messageForm = document.querySelector('#msg-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.getElementById('sendLocation')
+
+$messageForm.addEventListener('submit', (event) => {
     event.preventDefault()
+
+    $messageFormButton.disabled = true
     const msg = event.target.elements.msg
-    // console.log(msg.value)
-    socket.emit('sendMessage', msg.value)
-    msg.value = ''
+    socket.emit('sendMessage', msg.value, () => {
+        $messageFormButton.disabled = false
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+    })
 })
 
-document.querySelector('#sendLocation').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation services is not supported in your browser')
     }
+    $sendLocationButton.disabled = true
     navigator.geolocation.getCurrentPosition((position) => {
         const location = {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }
         socket.emit('sendLocation', location, () => {
+            $sendLocationButton.disabled = false
             console.log('Location shared')
         })
     })
