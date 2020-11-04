649f32a73b2d6d4d78411f164df3f31cc0ca8211
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-04 23:41:19 +0530

    Changed Timestamp string in util

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -3,7 +3,7 @@ const moment = require('moment')
 const generateMessage = (message) => {
     return {
         message,
-        createdAt: moment().format('MMMM Do YYYY, h:mm:ss a')
+        createdAt: moment().format('h:mm:ss a')
     }
 }
 module.exports = {
