810b895b55c08583e74e0881aecaf1f9ec31ab15
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-06 22:10:51 +0530

    received username in util

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,13 +1,15 @@
 const moment = require('moment')
 
-const generateMessage = (message) => {
+const generateMessage = (username, message) => {
     return {
+        username,
         message,
         createdAt: moment().format('h:mm:ss a')
     }
 }
-const generateLocation = (coords) => {
+const generateLocation = (username, coords) => {
     return {
+        username,
         url: `https://google.com/maps?q=${coords.latitude},${coords.longitude}`,
         createdAt: moment().format('h:mm:ss a')
     }
